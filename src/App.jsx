b312e612f5461af4d5d4b5a936246814af9fd124
Shivani Barman <shivanibarman247@gmail.com>
2026-01-30 22:25:54 +0530

    Add UserContext, Login, and Profile components; refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./context/UserContextprovider";
+
 
 function App() {
-  
-
   return (
-    
     <UserContextProvider>
-      <h1>Shivani is Amazing!!</h1>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
-
   );
 }
 
-export default App
+export default App;
